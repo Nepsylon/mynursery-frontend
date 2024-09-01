cfa11a480ec7f7cf39d2d3f57e0ae638c6f949fc
Simon Kinet <simon.kinet08@outlook.com>
2024-09-01 19:01:07 +0200

    Déploiement Vercel v9

--- src/app/environments/environment.secret.ts
+++ src/app/environments/environment.secret.ts
@@ -1,4 +1,3 @@
 export const secret = {
-    // RECAPTCHA_SITE_KEY: '6Le0wA8qAAAAAK04lFaZ9sy6pTWnfJQ0Awe_Hr_X',
-    RECAPTCHA_SITE_KEY: process.env.RECAPTCHA_SITE_KEY,
+    RECAPTCHA_SITE_KEY: '6Le0wA8qAAAAAK04lFaZ9sy6pTWnfJQ0Awe_Hr_X',
 };
