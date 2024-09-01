5a32259395a2140436237600101bccc5fd679252
Simon Kinet <simon.kinet08@outlook.com>
2024-09-01 18:58:35 +0200

    Déploiement Vercel v8

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
     production: true,
-    // URL: 'https://api.mynursery.site',
-    URL: process.env.URL || 'https://api.mynursery.site',
+    URL: 'https://api.mynursery.site',
 };
